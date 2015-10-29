a3bb317e0f0cf637c0f414bd86790ea760d757e2
Wolfgang Schuster <wolfadex@gmail.com>
2015-10-29 18:07:14 -0500

    Initial commit.

--- components/MainSection.js
+++ components/MainSection.js
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import PokemonList from './PokemonList';
 import FilterPokemon from './FilterPokemon';
+import LuaParse from '../libs/luaparse.js';
+import '../semantic/dist/semantic.min.css';
 
 export default class MainSection extends Component {
   static propTypes = {
@@ -12,19 +14,40 @@ export default class MainSection extends Component {
     const { data : { pokemon, caughtPokemon, searchTerm }, actions } = this.props;
 
     return (
-      <section className="main">
-        <h1>Pokedex in Redux</h1>
-
-        <FilterPokemon
-          searchTerm={searchTerm}
-          actions={actions} />
-
-        <PokemonList
-          pokemon={pokemon}
-          caughtPokemon={caughtPokemon}
-          actions={actions} />
-
-      </section>
+      <div className="ui attached stackable menu">
+          <div className="header item">
+              Lua Flow
+          </div>
+          <div className="ui simple dropdown item">
+              File
+              <i className="dropdown icon"></i>
+              <div className="menu">
+                  <a className="item">New</a>
+                  <a className="item">Open</a>
+                  <a className="item">Settings</a>
+                  <a className="item">Exit</a>
+              </div>
+          </div>
+          <div className="ui simple dropdown item">
+              Edit
+              <i className="dropdown icon"></i>
+              <div className="menu">
+                  <a className="item">Undo</a>
+                  <a className="item">Redo</a>
+                  <a className="item">Cut</a>
+                  <a className="item">Copy</a>
+                  <a className="item">Paste</a>
+              </div>
+          </div>
+          <div className="ui simple dropdown item">
+              View
+              <i className="dropdown icon"></i>
+              <div className="menu">
+                  <a className="item">Properties</a>
+                  <a className="item">Components</a>
+              </div>
+          </div>
+      </div>
     );
   }
 
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import App from './containers/App';
-import './css/bootstrap.css';
+// import './css/bootstrap.css';
+import './semantic/dist/semantic.min.css';
 import './css/index.css';
 
-React.render(
-  <App />,
-  document.getElementById('root')
-);
+React.render(<App />, document.getElementById('root'));
